14051ace1d46f3a011556d714edd63a467d3f587
Emma Adey <eadey@usf.edu>
2025-01-24 11:59:29 -0500

    Task 1- Employee Information

---
+++ cc_.1.js
@@ -0,0 +1,11 @@
+// Task 1 Variables //
+let employeeName = (JeighIves)
+const employeeID = "6543221"
+var isActive = true
+
+console.log(employeeName);
+console.log(typeof employeeName);
+console.log(employeeID);
+console.log(typeof employeeID);
+console.log(isActive); 
+console.log(typeof isActive);
