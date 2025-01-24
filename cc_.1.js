01ccfeb61894ea8489209c43cfd2ffb5e4586ba9
Emma Adey <eadey@usf.edu>
2025-01-24 12:00:55 -0500

    Task 2- Product Details

--- cc_.1.js
+++ cc_.1.js
@@ -9,3 +9,15 @@ console.log(employeeID);
 console.log(typeof employeeID);
 console.log(isActive); 
 console.log(typeof isActive);
+
+// Task 2 Primitive Data Types //
+let productName = "watches";
+const productPrice = 20;
+var isAvailable = true;
+
+console.log(productName);
+console.log(typeof productName);
+console.log(productPrice);
+console.log(typeof productPrice);
+console.log(isAvailable);
+console.log(typeof isAvailable);
